6a47085fec2bc5881806e5d6d9854dee5bd5f1ad
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-08-31 22:34:12 +0200

    added the sheety api and tested the POST

--- workout_tracking/main.py
+++ workout_tracking/main.py
@@ -1,9 +1,10 @@
 import requests
+from datetime import datetime 
 
 APP_ID = "app_6cd52af399ef4d85886b3c6c"
 API_KEY = "nix_live_ppY1AWVdoHzqQQzSKHGSntg1NBR1uiTW"
 ENDPOINT = "https://app.100daysofpython.dev/v1/nutrition/natural/exercise"
-# SHEET_ENDPOINT = 
+SHEETY_ENDPOINT = "https://api.sheety.co/901906ea70c3a57fdbeee98a6e24dbe9/tshepoMyWorkouts/workouts"
 GENDER = "male"
 WEIGHT = 64
 HEIGHT = 1.86
@@ -28,3 +29,21 @@ params = {
 response = requests.post(url=ENDPOINT, json=params, headers=headers)
 result = response.json()
 print(result)
+
+current_date = datetime.now().strftime("%d/%m/%Y")
+now = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheety_inputs = {
+        "workout":{
+            "date": current_date,
+            "time" : now,
+            "exercise": exercise["name"].title(),
+            "durations": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheety_response = requests.post(SHEETY_ENDPOINT, json=sheety_inputs)
+    sheety_results = sheety_response.json()
+    print(sheety_results)
