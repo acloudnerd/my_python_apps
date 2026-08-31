dc0bf9cf618016f0f2cc7e0f5b569c4a409e47b0
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-08-31 22:12:01 +0200

    added more params and tested the code, still need to fix the input

--- workout_tracking/main.py
+++ workout_tracking/main.py
@@ -3,6 +3,14 @@ import requests
 APP_ID = "app_6cd52af399ef4d85886b3c6c"
 API_KEY = "nix_live_ppY1AWVdoHzqQQzSKHGSntg1NBR1uiTW"
 ENDPOINT = "https://app.100daysofpython.dev/v1/nutrition/natural/exercise"
+# SHEET_ENDPOINT = 
+GENDER = "male"
+WEIGHT = "64"
+HEIGHT = "1.86"
+AGE = "27"
+
+
+input_text = input("What exercise(s) did you do? ")
 
 headers = {
     "x-app-key": API_KEY,
@@ -11,7 +19,12 @@ headers = {
 
 params = {
     "query": "indoor run for 10 mins",
-    "gender": "male"
+    "gender": "male",
+    "weight_kg": WEIGHT,
+    "height_cm": HEIGHT,
+    "age": AGE
 }
 
-response = requests.post(url=ENDPOINT, headers=headers)
+response = requests.post(url=ENDPOINT, json=params, headers=headers)
+result = response.json()
+print(result)
