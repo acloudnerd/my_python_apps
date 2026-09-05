a77a13e2115f70d30c815f17482e373c58516437
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-09-05 10:51:53 +0200

    configuring the flight data api

--- flight_deals/flight_data.py
+++ flight_deals/flight_data.py
@@ -1,3 +1,14 @@
+import requests
+
 class FlightData:
     #This class is responsible for structuring the flight data.
-    pass
+    FLIGHT_API = "https://serpapi.com/search?engine=google_flights"
+    
+    
+    def get_flight_data():
+    
+        params = {"departure_id": "CDG", "arrival_id": "AUS"}
+        
+        response = requests.get(self.FLIGHT_API, params=params)
+        
+        return response.json()
--- flight_deals/main.py
+++ flight_deals/main.py
@@ -1,7 +1,11 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
+from flight_data import FlightData
 
-data_manager = DataManager()
-data = data_manager.get_data()
+flight_data = FlightData()
+data = flight_data.get_flight_data()
+
+# data_manager = DataManager()
+# data = data_manager.get_data()
 
 print(data)
