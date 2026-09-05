92337ca273355145cdd213dcdd2734dc65853464
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-09-05 15:43:39 +0200

    flight data retrieved from the Serp API

--- flight_deals/flight_data.py
+++ flight_deals/flight_data.py
@@ -1,14 +1,31 @@
 import requests
+from dotenv import load_dotenv
+import os
+from datetime import date, timedelta
+
+load_dotenv()
 
 class FlightData:
     #This class is responsible for structuring the flight data.
-    FLIGHT_API = "https://serpapi.com/search?engine=google_flights"
-    
-    
-    def get_flight_data():
-    
-        params = {"departure_id": "CDG", "arrival_id": "AUS"}
-        
+    FLIGHT_API = "https://serpapi.com/search"
+
+    def __init__(self):
+        self.api_key = os.environ["API_KEY"]
+
+    def get_flight_data(self):
+        outbound_date = (date.today() + timedelta(days=30)).isoformat()
+        return_date = (date.today() + timedelta(days=37)).isoformat()
+
+        params = {
+            "engine": "google_flights",
+            "departure_id": "CDG",
+            "arrival_id": "AUS",
+            "outbound_date": outbound_date,
+            "return_date": return_date,
+            "api_key": self.api_key,
+        }
+
         response = requests.get(self.FLIGHT_API, params=params)
-        
+        response.raise_for_status()
+
         return response.json()
--- flight_deals/main.py
+++ flight_deals/main.py
@@ -2,10 +2,11 @@
 from data_manager import DataManager
 from flight_data import FlightData
 
+
 flight_data = FlightData()
 data = flight_data.get_flight_data()
 
-# data_manager = DataManager()
-# data = data_manager.get_data()
+data_manager = DataManager()
+data = data_manager.get_data()
 
 print(data)
