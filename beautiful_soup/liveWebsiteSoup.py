f3f6499d9c37ce492968174a97a287b9570200a5
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-09-15 22:42:12 +0200

    live website scraping demo

--- beautiful_soup/liveWebsiteSoup.py
+++ beautiful_soup/liveWebsiteSoup.py
@@ -10,4 +10,18 @@ first_title_span = soup.find("span", class_="titleline")
 
 first_article_link = first_title_span.find("a")
 
-print(first_article_link.text)
+first_article_text = first_article_link.text
+
+first_article_upvotes = soup.find(id="score_49700477", class_="score").text
+
+title_spans = soup.find_all(name="span", class_="titleline")
+
+article_texts = []
+article_links = []
+
+for title_span in title_spans:
+    article = title_span.find("a")
+    article_texts.append(article.get_text())
+    article_links.append(article.get("href"))
+
+print(article_texts)
